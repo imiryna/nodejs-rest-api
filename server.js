20bf8f0bf0a9973859cce0917b5ae27523ac4deb
Iryna Burlaka <i.am.blinova@gmail.com>
2024-04-28 18:02:24 +0200

    controller testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const utils = require("./utils");
 // import app from "./app.js";
 
-app.listen(3000, () => {
+module.exports = app.listen(3000, () => {
   utils.prepareEnvironment();
   console.log("Server running. Use our API on port: 3000");
 });
