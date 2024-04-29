80ffe862fcc236cd7e31155e6f46a413187d249e
Iryna Burlaka <i.am.blinova@gmail.com>
2024-04-29 11:25:45 +0200

    test auth/login

--- controllers/controller.test.js
+++ controllers/controller.test.js
@@ -2,4 +2,15 @@ const request = require("supertest");
 
 const app = require("../server");
 
-disc;
+discribe("test POST/auth/login", async () => {
+  beforeAll(console.log("BEFORE ALL"));
+
+  it("should return user obj and jwt");
+  const testData = {
+    //"should be mock fn"
+    email: "test@h.com",
+    password: "4352627",
+  };
+
+  const res = await request(app).post();
+});
